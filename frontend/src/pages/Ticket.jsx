a8ac2e7276afbd2c2072c9200c33c8817519ee47
yasithKumara <yasithkumara@gmail.com>
2023-07-10 03:22:02 +0530

    notes front done

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,68 +1,152 @@
-import React, { useEffect } from 'react'
-import { toast } from 'react-toastify'
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams, useNavigate } from 'react-router-dom'
-import Spinner from '../components/Spinner'
-import BackButton from '../components/BackButton'
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+import { getNotes, createNote, reset as notesReset } from "../features/notes/noteSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import NoteItem from "../components/NoteItem";
+import Modal from "react-modal";
+import { FaPlus } from "react-icons/fa";
+
+const customStyles = {
+  content: {
+    width: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    position: "relative",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState("");
 
-    const {ticket, isLoading, isSuccess, isError, message} = useSelector((state)=>state.ticket)
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.ticket
+  );
 
-    const params = useParams()
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const {ticketId} = useParams()
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.note
+  );
 
-    useEffect(()=>{
-        if(isError){
-            toast.error(message )
-        }
+  const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { ticketId } = useParams();
 
-        dispatch(getTicket(ticketId))
-    },[isError, message, ticketId])
-
-
-    if(isLoading){
-        return <Spinner/>
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
     }
 
-    if(isError){
-        return <div>Something went wrong</div>
-    }
+    dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
+  }, [isError, message, ticketId]);
 
-    const onTicketClose = () =>{
-        dispatch(closeTicket(ticketId))
-        toast.success('Ticket closed')
-        navigate('/tickets')
-    }
+  if (isLoading || notesIsLoading) {
+    return <Spinner />;
+  }
 
+  if (isError) {
+    return <div>Something went wrong</div>;
+  }
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket closed");
+    navigate("/tickets");
+  };
+
+  //create note submit
+  const onNoteSubmit = (e) =>{
+    e.preventDefault()
+    dispatch(createNote({noteText, ticketId}))
+    closeModal()
+  }
+
+  //open modal
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  //close modal
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
-    <div className='ticket-page'>
-        <header className='ticket-header'>
-            <BackButton url={'/tickets'}/>
-            <h2>
-                Ticket ID : {ticket._id}
-                <span className={`status status-${ticket.status}`}>{ticket.status}</span>
-            </h2>
-            <h3>
-                Date Submitted : {new Date(ticket.createdAt).toLocaleString('en-US')}
-            </h3>
-            <h3>
-                Product : {ticket.product}
-            </h3>
-            <hr />
-            <div className="ticket-desc">
-                <h3>Description of issue</h3>
-                <p>{ticket.description }</p> 
-            </div>
-        </header>
-        {ticket.status !== 'closed' && (
-            <button className="btn btn-block btn-danger" onClick={onTicketClose}>Close Ticket</button>
-        )}
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url={"/tickets"} />
+        <h2>
+          Ticket ID : {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted : {new Date(ticket.createdAt).toLocaleString("en-US")}
+        </h3>
+        <h3>Product : {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+
+      {ticket.status !== "closed" && (
+        <button className="btn" onClick={openModal}>
+          {" "}
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Note"
+      >
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className="form-group">
+            <textarea
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="note text"
+              value={noteText}
+              onChange={(e)=>setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">Submit</button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== "closed" && (
+        <button className="btn btn-block btn-danger" onClick={onTicketClose}>
+          Close Ticket
+        </button>
+      )}
     </div>
-   )
+  );
 }
 
-export default Ticket
+export default Ticket;
